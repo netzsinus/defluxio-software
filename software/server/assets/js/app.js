2933beb7161dfcbed3d2422062dffb4b0ee09243
Mathias Dalheimer <md@gonium.net>
2014-06-25 17:51:25 +0200

    Added cubism and dependencies - now working with asynchronous, randomly generated values. No integration w/ incomming frequency data yet.

--- software/server/assets/js/app.js
+++ software/server/assets/js/app.js
@@ -8,6 +8,23 @@ $(function() {
     
   }
 
+  function random(name) {
+    var value = 0,
+        values = [],
+        i = 0,
+        last;
+    return context.metric(function(start, stop, step, callback) {
+      start = +start, stop = +stop;
+      if (isNaN(last)) last = start;
+      while (last < stop) {
+        last += step;
+        value = Math.max(-10, Math.min(10, value + .8 * Math.random() - .4 + .2 * Math.cos(i += .2)));
+        values.push(value);
+      }
+      callback(null, values = values.slice((start - stop) / step));
+    }, name);
+  }
+
   if (window["WebSocket"]) {
     // initialize gauge
     var g = new JustGage({
@@ -20,7 +37,31 @@ $(function() {
         title: "Frequenz"
     }); 
     // initialize graph
-    // TODO
+    var context = cubism.context()
+        .serverDelay(0)
+        .clientDelay(0)
+        .step(2e3)
+        .size(960);
+
+    // TODO: Replace with ringbuffer thingie
+    var foo = random("foo"),
+        bar = random("bar");
+    // The graph updates itself asynchronously
+    d3.select("#chart").call(function(div) {
+      div.append("div")
+          .attr("class", "axis")
+          .call(context.axis().orient("top"));
+
+      div.selectAll(".horizon")
+          .data([foo, bar])
+        .enter().append("div")
+          .attr("class", "horizon")
+          .call(context.horizon().extent([-20, 20]));
+
+      div.append("div")
+          .attr("class", "rule")
+          .call(context.rule());
+    });
     // get data from the websocket.
     conn = new WebSocket(ws_endpoint);
     conn.onclose = function(evt) {
@@ -32,7 +73,7 @@ $(function() {
       g.refresh(data.Value);
       ts = new Date(Date.parse(data.Timestamp));
       $("#timevalue").text(ts.toLocaleTimeString());
-      // TODO: Update Graph
+      // TODO: Update ringbuffer
     }
 
     
